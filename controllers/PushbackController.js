af09a6a4789e4dae6c16d5d1a065a14b52970690
Jaskaran Singh <67426155+jaskaran5109@users.noreply.github.com>
2023-07-07 20:59:56 +0530

    Update PushbackController.js

--- controllers/PushbackController.js
+++ controllers/PushbackController.js
@@ -5,7 +5,7 @@ exports.getReports = catchAsyncError(async (req, res) => {
   const { offer, aff_sub2 } = req.body;
 
   const { data } = await axios.get(
-    `https://api.offer18.com/api/af/report?key=${process.env.OFFER18_API_KEY}&mid=${process.env.OFFER18_MID}&aid=${process.env.OFFER18_AID}&fields=clicks,conversion,offer_name,aff_sub,offer,aff_sub2,date&offer=${offer}&aff_sub2=${aff_sub2}`
+    `https://api.offer18.com/api/af/report?key=${process.env.OFFER18_API_KEY}&mid=${process.env.OFFER18_MID}&aid=${process.env.OFFER18_AID}&fields=clicks,affiliate_price,conversion,offer_name,aff_sub,offer,aff_sub2,date&offer=${offer}&aff_sub2=${aff_sub2}`
   );
 
   res.status(200).json({
